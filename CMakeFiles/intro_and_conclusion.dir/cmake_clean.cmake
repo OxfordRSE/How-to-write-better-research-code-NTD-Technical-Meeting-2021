a6d40551eec1e46613cd6d669eef72ed894aaedf
Thibault Lestang <thibault.lestang@cs.ox.ac.uk>
2021-03-23 09:08:35 +0000

    Generate slides

---
+++ CMakeFiles/intro_and_conclusion.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/intro_and_conclusion"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/intro_and_conclusion.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ CMakeFiles/slides.dir/cmake_clean.cmake
@@ -0,0 +1,5 @@
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/slides.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
